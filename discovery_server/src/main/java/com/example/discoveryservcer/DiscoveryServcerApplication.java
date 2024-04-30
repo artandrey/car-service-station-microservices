5d8e1e0bd8e362ace02469589d8a0a162972b71a
Artandrey <artandrey7777@gmail.com>
2024-04-30 22:26:09 +0300

    feat: created discovery server

---
+++ discovery_server/src/main/java/com/example/discoveryservcer/DiscoveryServcerApplication.java
@@ -0,0 +1,13 @@
+package com.example.discoveryservcer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServcerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServcerApplication.class, args);
+	}
+
+}
---
+++ discovery_server/src/test/java/com/example/discoveryservcer/DiscoveryServcerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.discoveryservcer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServcerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
