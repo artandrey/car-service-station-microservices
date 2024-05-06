62fd5d3412cf7369e5bf5016ede2657eee1f87f9
Artandrey <artandrey7777@gmail.com>
2024-05-06 23:39:07 +0300

    fix: set up proper config

--- discovery_server/src/main/java/com/example/discovery_server/SecurityConfig.java
+++ discovery_server/src/main/java/com/example/discovery_server/SecurityConfig.java
@@ -4,13 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(Customizer.withDefaults());
+        httpSecurity.csrf(new Customizer<CsrfConfigurer<HttpSecurity>>() {
+            public void customize(CsrfConfigurer<HttpSecurity> t) {
+                t.ignoringRequestMatchers("/eureka/**");
+            };
+        });
         return httpSecurity.build();
     }
 }
