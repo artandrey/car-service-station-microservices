ac58bf0d17ec0f41ab5d55de88387063fdb65ce4
Artandrey <artandrey7777@gmail.com>
2024-05-13 02:19:49 +0300

    feat: added delete endpoint

--- knowledge_base/src/main/java/com/example/knowledge_base/modules/documents_upload/controllers/FileUploadController.java
+++ knowledge_base/src/main/java/com/example/knowledge_base/modules/documents_upload/controllers/FileUploadController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,11 @@ public class FileUploadController {
                 .contentType(MediaType.parseMediaType(fileRecord.getMimeType()))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileRecord.getFilePath() + "\"")
                 .body(new ByteArrayResource(fileRecord.getFileContent()));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable String id) {
+        fileUploadService.deleteById(id);
+        return ResponseEntity.accepted().build();
     }
 }
