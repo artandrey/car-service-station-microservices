4f03a26c02c122a248a1e94d7c389b3bf3c086f9
Artandrey <artandrey7777@gmail.com>
2024-05-13 02:44:12 +0300

    fix: added missing component annotation

--- knowledge_base/src/main/java/com/example/knowledge_base/modules/documents_upload/mapper/DocumentMapper.java
+++ knowledge_base/src/main/java/com/example/knowledge_base/modules/documents_upload/mapper/DocumentMapper.java
@@ -1,12 +1,14 @@
 package com.example.knowledge_base.modules.documents_upload.mapper;
 
 import org.springframework.ai.document.Document;
+import org.springframework.stereotype.Component;
 
 import com.example.knowledge_base.modules.documents_upload.dto.DocumentResponseDto;
 
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
+@Component
 public class DocumentMapper {
 
     public DocumentResponseDto toDto(Document document) {
