e441aa5ee877b2fcddd03ba14e9bc0da7a113d5d
Artandrey <artandrey7777@gmail.com>
2024-05-14 21:55:49 +0300

    feat: added model mapper bean to ai app

---
+++ ai_assistant/src/main/java/com/example/ai_assistant/config/AiAssistantConfig.java
@@ -0,0 +1,21 @@
+package com.example.ai_assistant.config;
+
+import org.modelmapper.Conditions;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AiAssistantConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setPropertyCondition(Conditions.isNotNull())
+                .setSkipNullEnabled(true);
+
+        return modelMapper;
+    }
+}
